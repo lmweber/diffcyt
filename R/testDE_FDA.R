96894b6f9bb8282f20b75553e2c39f5941016efa
Lukas Weber <lukmweber@gmail.com>
2017-03-03 20:34:02 +0100

    parallelization with BiocParallel

--- R/testDE_FDA.R
+++ R/testDE_FDA.R
@@ -19,6 +19,9 @@
 #' steps; with modifications to the permutation t-testing function to allow weighting and
 #' improve runtime.
 #' 
+#' The \code{\link[BiocParallel]{BiocParallel}} package is used for parallelized 
+#' evaluation on multi-core systems, to speed up runtime.
+#' 
 #' Alternative methodologies for testing for differential expression of functional markers
 #' are available in the functions \code{testDE_med} and \code{testDE-KS}.
 #' 
@@ -39,6 +42,9 @@
 #' @param n_perm Number of permutations to use for permutation testing. Default = 5000 
 #'   (i.e. minimum possible p-value = 0.0002).
 #' 
+#' @param n_cores Number of processor cores for parallelized evaluation. Default = all
+#'   available cores.
+#' 
 #' 
 #' @return Returns a data frame containing cluster labels, functional marker names, and 
 #'   p-values summarizing the evidence for differential expression. Rows are sorted by 
@@ -48,6 +54,7 @@
 #' @importFrom SummarizedExperiment assays rowData colData
 #' @importFrom fda Data2fd
 #' @importFrom reshape2 melt
+#' @importFrom BiocParallel bplapply MulticoreParam multicoreWorkers
 #' 
 #' @export
 #' 
@@ -108,7 +115,7 @@
 #' # are not biologically meaningful)
 #' head(res_DE_FDA)
 #' 
-testDE_FDA <- function(d_ecdfs, d_clus, group, n_perm = 5000) {
+testDE_FDA <- function(d_ecdfs, d_clus, group, n_perm = 5000, n_cores = NULL) {
   
   if (!is.factor(group)) group <- factor(group, levels = unique(group))
   
@@ -136,39 +143,53 @@ testDE_FDA <- function(d_ecdfs, d_clus, group, n_perm = 5000) {
   weights1 <- weights[, grp]
   weights2 <- weights[, !grp]
   
-  # calculate p-values
+  
+  # function for parallelized evaluation: calculates p-value for cluster 'i'
+  eval_iter_i <- function(i, assays_j, resolution, smp, argvals, grp, weights1, weights2, n_perm) {
+    
+    y <- assays_j[i, ]
+    
+    # fix missing values (when zero cells in this cluster-sample combination)
+    # [to do: move this to 'calcECDFs()']
+    y <- lapply(y, function(yy) {
+      if (length(yy) == 0) yy <- rep(0, resolution)
+      yy
+    })
+    
+    y <- matrix(unlist(y), ncol = length(smp))
+    
+    fd1 <- Data2fd(argvals = argvals, y = y[, grp])
+    fd2 <- Data2fd(argvals = argvals, y = y[, !grp])
+    
+    # note: keeping p-values only (discarding all other results)
+    # note: error handling: may return errors for some iterations; return NA in these cases
+    p_val <- tryCatch({
+      .tperm.fd_wtd_fast(fd1, fd2, weights1[i, ], weights2[i, ], nperm = n_perm, plotres = FALSE)$pval
+    }, error = function(e) e)
+    
+    if (is(p_val, "simpleError")) p_val <- NA
+    
+    p_val
+  }
+  
+  
+  # BiocParallel parameters
+  if (is.null(n_cores)) n_cores <- multicoreWorkers()
+  bpparam <- MulticoreParam(workers = n_cores)
+  
+  # calculate p-values (parallelized for each marker 'j')
   # [to do: include some filtering: no. of cells, no. of samples]
   for (j in seq_along(func_markers)) {
     
     assays_j <- assays(d_ecdfs)[[j]]
     
-    for (i in seq_along(clus)) {
-      cat(".")
-      y <- assays_j[i, ]
-      
-      # fix missing values (when zero cells in this cluster-sample combination)
-      # [to do: move this to 'calcECDFs()']
-      y <- lapply(y, function(yy) {
-        if (length(yy) == 0) yy <- rep(0, resolution)
-        yy
-      })
-      
-      y <- matrix(unlist(y), ncol = length(smp))
-      
-      fd1 <- Data2fd(argvals = argvals, y = y[, grp])
-      fd2 <- Data2fd(argvals = argvals, y = y[, !grp])
-      
-      # note: keeping p-values only (discarding all other results)
-      # note: error handling: may return errors for some iterations; return NA in these cases
-      p_val <- tryCatch({
-        .tperm.fd_wtd_fast(fd1, fd2, weights1[i, ], weights2[i, ], nperm = n_perm, plotres = FALSE)$pval
-      }, error = function(e) e)
-      
-      if (is(p_val, "simpleError")) p_val <- NA
-      
-      p_vals[i, j] <- p_val
-    }
+    p_vals[, j] <- unlist(
+      bplapply(seq_along(clus), eval_iter_i, assays_j, resolution, smp, argvals, grp, weights1, weights2, n_perm)
+    )
+    
+    cat("marker", j, "complete\n")
   }
+  
   
   # sort to show lowest p-values (across functional markers and clusters) at the top
   res <- as.data.frame(p_vals)
