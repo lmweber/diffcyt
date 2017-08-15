0f6272e3dc68c111e143fa8f72f047c7fb6d8ae3
Lukas Weber <lukmweber@gmail.com>
2017-08-15 17:08:13 +0200

    add method 'diffcyt-DE-med'

---
+++ R/testDE_med.R
@@ -0,0 +1,211 @@
+#' Test for differential expression within clusters: method 'diffcyt-med'
+#' 
+#' Calculate tests for differential expression of functional markers within clusters using
+#' method 'diffcyt-med'.
+#' 
+#' This method uses median marker expression values to characterize the functional marker
+#' expression profiles within each cluster (i.e. the signal of interest). In addition,
+#' empirical Bayes methods are used to improve statistical power by sharing information on
+#' variability (i.e. variance across samples for a single cluster) between clusters. The
+#' \code{\link[limma]{limma}} package (Ritchie et al. 2015, \emph{Nucleic Acids Research})
+#' is used to fit linear models and calculate empirical Bayes moderated tests. Since count
+#' data are often heteroscedastic, we use the  \code{\link[limma]{voom}} method (Law et
+#' al. 2014, \emph{Genome Biology}) to transform the raw cluster cell counts and estimate
+#' observation-level weights to stabilize the mean-variance relationship. Diagnostic plots
+#' are shown if \code{plot = TRUE}.
+#' 
+#' The experimental design must be specified using a design matrix, which can be created
+#' with \code{\link{createDesignMatrix}}. Flexible experimental designs are possible,
+#' including batch effects, continuous covariates, and paired designs. See
+#' \code{\link{createDesignMatrix}} for more details.
+#' 
+#' For paired designs, either fixed effects or random effects can be used. Fixed effects
+#' are simpler, but random effects may improve power for unbalanced designs. To use fixed
+#' effects, provide the block IDs (e.g. patient IDs) to \code{\link{createDesignMatrix}}.
+#' To use random effects, provide the \code{block_IDs} argument here instead. This will
+#' make use of the \code{limma} \code{\link[limma]{duplicateCorrelation}} methodology;
+#' note that block IDs should not be included in the design matrix in this case.
+#' 
+#' The contrast matrix specifying the contrast of interest can be created with
+#' \code{\link{createContrast}}. See \code{\link{createContrast}} for more details.
+#' 
+#' Filtering: Clusters are kept for differential testing if they have at least
+#' \code{min_cells} cells in at least \code{min_samples} samples in at least one
+#' condition. This removes clusters with very low cell counts across conditions, which
+#' improves power. In addition, clusters containing zero cells for any sample are removed,
+#' due to the requirements of \code{voom}.
+#' 
+#' 
+#' @param d_se Data object from previous steps, in
+#'   \code{\link[SummarizedExperiment]{SummarizedExperiment}} format, containing marker
+#'   information in column meta-data.
+#' 
+#' @param d_counts \code{\link[SummarizedExperiment]{SummarizedExperiment}} object
+#'   containing cluster cell counts, from \code{\link{calcCounts}}.
+#' 
+#' @param d_medians \code{\link[SummarizedExperiment]{SummarizedExperiment}} object
+#'   containing cluster medians (median expression of functional markers), from
+#'   \code{\link{calcMedians}}.
+#' 
+#' @param design Design matrix, created with \code{\link{createDesignMatrix}}. See
+#'   \code{\link{createDesignMatrix}} for details.
+#' 
+#' @param contrast Contrast matrix, created with \code{\link{createContrast}}. See
+#'   \code{\link{createContrast}} for details.
+#' 
+#' @param block_IDs (Optional) Vector or factor of block IDs (e.g. patient IDs) for paired
+#'   experimental designs, to be included as random effects. If provided, the block IDs
+#'   will be included as random effects using the \code{limma}
+#'   \code{\link[limma]{duplicateCorrelation}} methodology. Alternatively, block IDs can
+#'   be included as fixed effects in the design matrix (\code{\link{createDesignMatrix}}).
+#'   See details.
+#' 
+#' @param min_cells Filtering parameter. Default = 3. Clusters are kept for differential
+#'   testing if they have at least \code{min_cells} cells in at least \code{min_samples}
+#'   samples in at least one condition. In addition, clusters containing zero cells for
+#'   any sample are removed, due to the requirements of \code{voom}.
+#' 
+#' @param min_samples Filtering parameter. Default = \code{min(table(group_IDs)) - 1},
+#'   i.e. one less than the number of samples in the smallest group. Clusters are kept for
+#'   differential testing if they have at least \code{min_cells} cells in at least
+#'   \code{min_samples} samples in at least one condition. In addition, clusters
+#'   containing zero cells for any sample are removed, due to the requirements of
+#'   \code{voom}.
+#' 
+#' @param plot Whether to save diagnostic plots for the \code{limma}
+#'   \code{\link[limma]{voom}} transformations. Default = TRUE.
+#' 
+#' @param path Path for diagnostic plots. Default = current working directory.
+#' 
+#' 
+#' @return Returns a new \code{\link[SummarizedExperiment]{SummarizedExperiment}} object,
+#'   where rows = cluster-marker combinations, columns = samples. In the rows, clusters
+#'   are repeated for each functional marker (i.e. the sheets or 'assays' from the
+#'   previous \code{d_medians} object are stacked into a single matrix). Differential test
+#'   results are stored in the 'rowData' slot. Results include raw p-values and adjusted
+#'   p-values from the \code{\link[limma]{limma}} empirical Bayes moderated tests, which
+#'   can be used to rank cluster-marker combinations by evidence for differential
+#'   expression. The results can be accessed with the
+#'   \code{\link[SummarizedExperiment]{rowData}} accessor function.
+#' 
+#' 
+#' @importFrom SummarizedExperiment assays rowData colData
+#' @importFrom limma contrasts.fit voom duplicateCorrelation lmFit eBayes plotSA topTable
+#' @importFrom methods as is
+#' @importFrom grDevices pdf
+#' @importFrom graphics plot
+#' 
+#' @export
+#' 
+#' @examples
+#' # to do
+#' 
+testDE_med <- function(d_se, d_counts, d_medians, design, contrast, 
+                       block_IDs = NULL, 
+                       min_cells = 3, min_samples = NULL, 
+                       plot = TRUE, path = ".") {
+  
+  if (!is.null(block_IDs) & !is.factor(block_IDs)) {
+    block_IDs <- factor(block_IDs, levels = unique(block_IDs))
+  }
+  
+  group_IDs <- colData(d_counts)$group
+  
+  if (is.null(min_samples)) {
+    min_samples <- min(table(group_IDs)) - 1
+  }
+  
+  counts <- assays(d_counts)[[1]]
+  cluster <- rowData(d_counts)$cluster
+  
+  # filtering: keep clusters with at least 'min_cells' cells in at least 'min_samples'
+  # samples in at least one condition
+  ix_keep <- rep(FALSE, length(cluster))
+  tf <- counts >= min_cells
+  for (g in seq_along(levels(group_IDs))) {
+    grp <- group_IDs == levels(group_IDs)[g]
+    ix_keep[rowSums(tf[, grp, drop = FALSE]) >= min_samples] <- TRUE
+  }
+  
+  counts <- counts[ix_keep, ]
+  cluster <- cluster[ix_keep]
+  
+  # remove any remaining rows with zeros (required by voom)
+  ix_zeros <- apply(counts, 1, function(r) any(r == 0))
+  
+  counts <- counts[!ix_zeros, ]
+  cluster <- cluster[!ix_zeros]
+  
+  # extract medians and create concatenated matrix
+  func_names <- as.character(colData(d_se)$markers[colData(d_se)$is_DE_col])
+  meds <- do.call("rbind", {
+    lapply(as.list(assays(d_medians)[func_names]), function(a) a[cluster, ])
+  })
+  
+  meds_all <- do.call("rbind", as.list(assays(d_medians)[func_names]))
+  
+  # voom transformation and weights
+  if (plot) pdf(file.path(path, "voom_before.pdf"), width = 6, height = 6)
+  v <- voom(meds, design, plot = TRUE)
+  if (plot) dev.off()
+  
+  # estimate correlation between paired samples
+  # (note: paired designs only; >2 measurements per sample not allowed)
+  if (!is.null(block_IDs)) {
+    dupcor <- duplicateCorrelation(v, design, block = block_IDs)
+  }
+  
+  # fit linear models
+  if (!is.null(block_IDs)) {
+    vfit <- lmFit(v, design, block = block_IDs, correlation = dupcor$consensus.correlation)
+  } else {
+    vfit <- lmFit(v, design)
+  }
+  vfit <- contrasts.fit(vfit, contrast)
+  
+  # calculate empirical Bayes moderated tests
+  efit <- eBayes(vfit)
+  
+  if (plot) pdf(file.path(path, "voom_after.pdf"), width = 6, height = 6)
+  plotSA(efit)
+  if (plot) dev.off()
+  
+  # results
+  top <- topTable(efit, coef = 1, number = Inf, adjust.method = "BH", sort.by = "none")
+  
+  # return results in 'rowData' of new 'SummarizedExperiment' object
+  
+  # fill in any missing rows (filtered clusters) with NAs
+  row_data <- as.data.frame(matrix(as.numeric(NA), 
+                                   nrow = nlevels(cluster) * length(func_names), 
+                                   ncol = ncol(top)))
+  colnames(row_data) <- colnames(top)
+  
+  cluster_nm <- as.numeric(cluster)
+  s <- seq(0, nlevels(cluster) * (length(func_names) - 1), by = nlevels(cluster))
+  r1 <- rep(cluster_nm, length(func_names))
+  r2 <- rep(s, each = length(cluster_nm))
+  
+  stopifnot(length(s) == length(func_names))
+  stopifnot(length(r1) == length(r2))
+  
+  rows <- r1 + r2
+  row_data[rows, ] <- top
+  
+  # include cluster IDs and marker names
+  clus <- factor(rep(levels(cluster), length(func_names)), levels = levels(cluster))
+  func <- factor(rep(func_names, each = length(levels(cluster))), levels = func_names)
+  stopifnot(length(clus) == nrow(row_data), length(func) == nrow(row_data))
+  
+  row_data <- cbind(data.frame(cluster = clus, marker = func), row_data)
+  
+  # store additional sample information in 'colData'
+  col_data <- cbind(colData(d_medians), data.frame(block_IDs))
+  
+  # return object
+  res <- SummarizedExperiment(meds_all, rowData = row_data, colData = col_data)
+  
+  res
+}
+
+
